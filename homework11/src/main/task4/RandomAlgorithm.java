08c15bc12d01c7dcc540eae71b2c26d19d1c2c47
RomanRokitskyi <99216328+RomanRokitski@users.noreply.github.com>
2023-01-16 15:56:33 +0200

    Update RandomAlgorithm.java

--- homework11/src/main/task4/RandomAlgorithm.java
+++ homework11/src/main/task4/RandomAlgorithm.java
@@ -4,19 +4,17 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class RandomAlgorithm {
-    
-public static void main(String[] args) {
-    RandomAlgorithm r = new RandomAlgorithm();
+    public static void main(String[] args) {
+        RandomAlgorithm r = new RandomAlgorithm();
 
-    r.getRandom(25214903917L, 11, 2 ^ 48L)
-            .limit(10)
-            .peek(System.out::println)
-            .collect(Collectors.toList());
-}
+        r.getRandom(25214903917L, 11, 2 ^ 48L)
+                .limit(10)
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+    }
+
     public Stream<Long> getRandom(long a, long c, long m) {
 
         return Stream.iterate(0L, x -> (a * x + c) % m);
-
     }
-
 }
